7bc9d261762d10b569d53ab0b6de5e325183c723
AkbarMaryanB <100180116+AkbarMaryanB@users.noreply.github.com>
2022-03-01 11:57:50 +0700

    Add files via upload

---
+++ Praktikum 2/Luas Bujur Sangkar.c
@@ -0,0 +1,24 @@
+/*Nama File     : Luas Bujur Sangkar*/
+/*Deskripsi     : Menghitung Luas Bujur Sangkar*/
+/*Pembuat       : Akbar Maryan Bagaskara-24060121120019*/
+/*Tgl Pembuatan : 1 Maret 2022*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    /*kamus*/
+    int S;
+    int L;
+
+    /*Algoritma*/
+    scanf("%d", &S);
+    L = S*S;
+    printf("Sisi = %d\n", S);
+    printf("Luas Bujur Sangkar = %d", L);
+
+    return 0;
+}
+
+
